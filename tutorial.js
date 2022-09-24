9d4a0f3f929b419a435ea5e785c9842b9db71fcf
PaolaFoc <82398421+PaolaFoc@users.noreply.github.com>
2022-09-24 19:56:17 +0200

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -1,10 +1,10 @@
 import * as THREE from './three.js-master/build/three.module.js';
-import * as GLTF from '/tree/main/three.js-master/examples/jsm/loaders/GLTFLoader.js';
-import * as LOADER from '/tree/main/three.js-master/src/loaders/LoadingManager.js';
-import * as UTIL from '/tree/main/common/utilities';
-import * as ANIMATION from '/tree/main/common/animation';
-import {Racoon} from '/tree/main/common/player';
-import {World} from '/tree/main/common/world';
+import * as GLTF from './three.js-master/examples/jsm/loaders/GLTFLoader.js';
+import * as LOADER from './three.js-master/src/loaders/LoadingManager.js';
+import * as UTIL from './common/utilities';
+import * as ANIMATION from './common/animation';
+import {Racoon} from './common/player';
+import {World} from './common/world';
 
 
 var racoon;
