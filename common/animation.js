fa90eaa7789f4cd514f8da3866604096d064904b
PaolaFoc <82398421+PaolaFoc@users.noreply.github.com>
2022-09-24 20:32:21 +0200

    Update animation.js

--- common/animation.js
+++ common/animation.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.138.0/build/three.module.js';
+import * as THREE from '../three.js-master/build/three.module.js';
 
 var groundDistance=0;
 var objects=[];
